145c9060a179ba0ea546194a977ef67ad68c383e
runsisi <runsisi@hust.edu.cn>
2016-02-16 00:56:02 +0800

    update install.sh
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 systemctl stop tcmu-runner > /dev/null 2>&1
 
 install -d /usr/lib/tcmu-runner
-install handler_* /usr/lib/tcmu-runner
+install handler_* /usr/lib64/tcmu-runner
 install libtcmu.so.1 /usr/lib/
 install libtcmu.so /usr/lib/
 
-cp -f tcmu-runner /usr/bin/
-cp -f tcmu-runner.conf /etc/dbus-1/system.d/
-cp -f org.kernel.TCMUService1.service /usr/share/dbus-1/system-services/
-cp -f tcmu-runner.service /lib/systemd/system/
+install tcmu-runner /usr/bin/
+install tcmu-runner.conf /etc/dbus-1/system.d/
+install org.kernel.TCMUService1.service /usr/share/dbus-1/system-services/
+install tcmu-runner.service /lib/systemd/system/
 
 systemctl daemon-reload
 
